7206dba45ad9d15bb774dd5910ed0505473966b6
ahmedabdullahfahim <ahmed.abdallah5022@gmail.com>
2023-01-23 19:06:10 +0200

    removed some unnecessary padding

--- components/Post.js
+++ components/Post.js
@@ -121,7 +121,7 @@ const Post = ({ id, name, userImg, postImg, caption }) => {
           <BookmarkIcon className='headerIcon h-7' />
         </div>
       )}
-      <p className='p-5 truncate'>
+      <p className='px-5 truncate'>
         {likes.length > 0 && (
           <p className='font-bold mb-1'>
             {likes.length}
