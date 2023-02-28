529a90839bf1726e4ff7c6a519a6b314a3099939
ahmedabdullahfahim <ahmed.abdallah5022@gmail.com>
2023-02-28 22:35:28 +0200

    added logo

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div className='bg-gray-50 h-screen overflow-y-scroll scrollbar-hide'>
       <Head>
         <title>Instagram</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/logo.png' />
       </Head>
 
       <Header />
